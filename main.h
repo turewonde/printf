6e5d2cad568cbc0dd18dc782ef3a9540c247d022
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2021-10-05 06:34:45 -0700

    wonde printf

--- main.h
+++ main.h
@@ -3,24 +3,31 @@
 
 #include <stdarg.h>
 
-/**
-* struct convert - defines a structure for symbols and functions
-*
-* @sym: The operator
-* @f: The function associated
-*/
-struct convert
-{
-	char *sym;
-	int (*f)(va_list);
-};
-typedef struct convert conver_t;
-
-int wonde(const char *format, conver_t f_list[], va_list arg_list);
 int _putchar(char c);
+int wonde_int(va_list arg);
+int wonde_unsigned(va_list arg);
 int _printf(const char *format, ...);
 int wonde_char(va_list arg);
 int wonde_str(va_list arg);
-int wonde_percent(__attribute__((unused))va_list list);
+int wonde_percent(void);
+void wonde_binary(unsigned int n, unsigned int* printed);
+int wonde_unsignedToBinary(va_list arg);
+int wonde_oct(va_list arg);
+int wonde_unsignedIntToHex(unsigned int num, char _case);
+int wonde_hex_base(va_list arg, char _case);
+int wonde_hex(va_list arg);
+int wonde_HEX(va_list arg);
+int wonde_STR (va_list arg);
+
+/**
+ * struct identifierStruct - structure definition of a printTypeStruct
+ * @indentifier: type
+ * @printer: function to print
+ */
+typedef struct identifierStruct
+{
+char *indentifier;
+int (*printer)(va_list);
+} identifierStruct;
 
 #endif
