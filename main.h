5fdc1ae9d6de886959e516d6812f5f929b5ec585
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2021-10-05 05:43:14 -0700

    wonde first

--- main.h
+++ main.h
@@ -5,5 +5,8 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int wonde_char(va_list arg);
+int wonde_str(va_list arg);
+int wonde_percent(__attribute__((unused))va_list list);
 
 #endif
