50995b22eaaa9fe0b0f4070b21c3983afc36c1ca
Wondmagegn Rahmeto <tirusewalem2021@gmail.com>
2021-10-07 00:20:27 -0700

    character

--- wonde_first.c
+++ wonde_first.c
@@ -2,33 +2,115 @@
 #include <stdlib.h>
 
 /**
- * print_c - Print a character.
- * @arg: Char to be printed.
- * Return: On success 1.
- * On error -1.
- **/
-
-int print_c(va_list arg)
+ * print_c - prints a char
+ * @c: char to print
+ *
+ * Return: always 1
+ */
+int print_c(va_list c)
 {
-	char c = va_arg(arg, int);
+	char ch = (char)va_arg(c, int);
 
-	return (_putchar(c));
+	_putchar(ch);
+	return (1);
 }
 
 /**
- * print_s - Print a string.
- * @arg: String address.
- * Return: integer.
+ * print_s - prints a string
+ * @s: string to print
+ *
+ * Return: number of chars printed
  */
-int print_s(va_list arg)
+int print_s(va_list s)
 {
-	char *str = va_arg(arg, char *);
-	int i = 0;
+	int count;
+	char *str = va_arg(s, char *);
 
 	if (str == NULL)
 		str = "(null)";
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+	return (count);
+}
 
-	for (; str[i]; i++)
+/**
+ * hex_print - prints a char's ascii value in uppercase hex
+ * @c: char to print
+ *
+ * Return: number of chars printed (always 2)
+ */
+static int hex_print(char c)
+{
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
+}
+
+/**
+ * print_S - prints a string and nonprintable character ascii values
+ * @S: string to print
+ *
+ * Return: number of chars printed
+ */
+int print_S(va_list S)
+{
+	unsigned int i;
+	int count = 0;
+	char *str = va_arg(S, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			count += 2;
+			count += hex_print(str[i]);
+		}
+		else
+		{
+			_putchar(str[i]);
+			count++;
+		}
+	}
+	return (count);
+}
+
+/**
+ * print_r - prints astring in reverse
+ * @r: string to print
+ *
+ * Return: number of chars printed
+ */
+int print_r(va_list r)
+{
+	char *str;
+	int i, count = 0;
+
+	str = va_arg(r, char *);
+	if (str == NULL)
+		str = ")llun(";
+	for (i = 0; str[i]; i++)
+		;
+	for (i -= 1; i >= 0; i--)
+	{
 		_putchar(str[i]);
-	return (i);
+		count++;
+	}
+	return (count);
 }
