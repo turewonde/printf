59e4df6a8dc64ca6762fec6c8349ee607d4bd2d0
Bikila boja <bekyboja99@gmail.com.com>
2021-10-06 23:55:02 -0700

    this is main.h

--- main.h
+++ main.h
@@ -1,28 +1,32 @@
-#ifndef PRINTF_H
-#define PRINTF_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdarg.h>
 /**
- * struct form - Types.
- * @c: Types in c.
- * @f: Corresponding function.
- **/
-typedef struct form
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
 {
-char c;
-int (*f)(va_list);
-} type_printer;
-int _printf(const char *format, ...);
-int print_s(va_list arg);
-int print_c(va_list arg);
-int print_i(va_list arg);
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
 int _putchar(char c);
-int print_rot13(va_list arg);
-int print_b(va_list arg);
-int print_r(va_list arg);
-int print_S(va_list arg);
-int print_p(va_list arg);
-int print_u(va_list arg);
-int print_x(va_list arg);
-int print_X(va_list arg);
-int print_o(va_list arg);
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
+
 #endif
